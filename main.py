7cb0deebf15a47723d91a7fd045b576454c6e586
wulayu <849519573@qq.com>
2022-07-25 16:11:32 +0800

    d2lzh_pytorch

--- main.py
+++ main.py
@@ -4,6 +4,20 @@ from matplotlib import pyplot as plt
 import matplotlib_inline.backend_inline
 import numpy as np
 import random
+import sys
+
+sys.path.append("D://")  # 为了导入上层目录的d2lzh_pytorch
+import d2lzh_pytorch as d2l
+
+# def use_svg_display():
+#     # 用矢量图显示
+#     matplotlib_inline.backend_inline.set_matplotlib_formats('svg')
+#
+#
+# def set_figsize(figsize=(7, 5)):
+#     use_svg_display()
+#     # 设置图的尺寸
+#     plt.rcParams['figure.figsize'] = figsize
 
 num_inputs = 2
 num_examples = 1000
@@ -13,19 +27,7 @@ features = torch.randn(num_examples, num_inputs, dtype=torch.float32)
 labels = true_w[0] * features[:, 0] + true_w[1] * features[:, 1] + true_b
 labels += torch.tensor(np.random.normal(0, 0.01, size=labels.size()), dtype=torch.float32)
 
-
-def use_svg_display():
-    # 用矢量图显示
-    matplotlib_inline.backend_inline.set_matplotlib_formats('svg')
-
-
-def set_fig_size(fig_size=(7, 5)):
-    use_svg_display()
-    # 设置图的尺寸
-    plt.rcParams['figure.figsize'] = fig_size
-
-
-set_fig_size()
+d2l.set_figsize(figsize=(7, 5))
 plt.scatter(features[:, 1].numpy(), labels.numpy(), 1)
 
 plt.show()
